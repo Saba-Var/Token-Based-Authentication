46b1f92c4817c87cfe54899e51e8c15040a99664
Saba-Var <vartasashvili94@gmail.com>
2023-10-08 22:21:09 +0400

    chore: create cypress env file

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,7 +7,4 @@ export default defineConfig({
     viewportWidth: 1920,
     baseUrl: 'http://localhost:5173',
   },
-  env: {
-    API_BASE_URI: 'https://token-based-authentication-api.vercel.app',
-  },
 })
