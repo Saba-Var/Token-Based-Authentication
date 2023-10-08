f63157e59966c1049d6363d29ef929096f2c85ec
Saba-Var <vartasashvili94@gmail.com>
2023-10-08 14:36:08 +0400

    feat: integrate react toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import ReactDOM from 'react-dom/client'
 import { router } from '@/router'
 import React from 'react'
@@ -14,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={router} />
+      <ToastContainer />
     </QueryClientProvider>
   </React.StrictMode>,
 )
---
+++ src/utils/emitToast.ts
@@ -0,0 +1,13 @@
+import { toast } from 'react-toastify'
+
+export const emitToast = (message: string, type: 'success' | 'error' = 'success') => {
+  return toast[type](message, {
+    position: 'top-right',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  })
+}
