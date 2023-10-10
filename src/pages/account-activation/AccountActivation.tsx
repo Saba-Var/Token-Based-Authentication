9c159c63f900189a46812af254698ff6d69aff65
Saba-Var <vartasashvili94@gmail.com>
2023-10-10 10:37:15 +0400

    feat: account activation page ui

--- src/pages/account-activation/AccountActivation.tsx
+++ src/pages/account-activation/AccountActivation.tsx
@@ -1,9 +1,32 @@
 import useActivationRequest from './useAccountActivation'
+import { LoadingIcon } from '@/components'
+import { Link } from 'react-router-dom'
 
 const AccountActivation = () => {
-  useActivationRequest()
+  const { isLoading, activationResultToShow, t } = useActivationRequest()
 
-  return <div>AccountActivation</div>
+  return (
+    <div className='h-screen bg-white w-full flex justify-center'>
+      {!isLoading && activationResultToShow?.image ? (
+        <div className='flex items-center justify-center flex-col gap-5'>
+          <div>
+            <img
+              className='h-[50vh] w-full'
+              src={activationResultToShow.image}
+              alt='activation result'
+            />
+          </div>
+          <h2 className='text-xl sm:text-3xl px-2 text-center'>{activationResultToShow.text}</h2>
+
+          <Link replace to={activationResultToShow.redirect} className='primary-btn'>
+            {activationResultToShow.redirect === '/' ? t('go_back_home') : t('log_in')}
+          </Link>
+        </div>
+      ) : (
+        <LoadingIcon centered />
+      )}
+    </div>
+  )
 }
 
 export default AccountActivation
--- src/pages/account-activation/useAccountActivation.ts
+++ src/pages/account-activation/useAccountActivation.ts
@@ -1,7 +1,9 @@
 import { useSearchParams, useLocation, useNavigate } from 'react-router-dom'
+import { teamDiscussing, celebration, stressedMan } from '@/assets'
 import { accountActivationRequest } from '@/services'
+import { useEffect, useMemo, useState } from 'react'
 import { useMutation } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
 const useActivationRequest = () => {
   const [activationStatusCode, setActivationStatusCode] = useState<null | number>(null)
@@ -9,16 +11,22 @@ const useActivationRequest = () => {
   const [searchParams] = useSearchParams()
   const location = useLocation()
   const navigate = useNavigate()
+  const { t } = useTranslation()
 
-  const { mutate: activateAccountMutation } = useMutation(accountActivationRequest, {
+  const { mutate: activateAccountMutation, isLoading } = useMutation(accountActivationRequest, {
     onSuccess: (data) => {
       navigate(location.pathname, { replace: true })
       setActivationStatusCode(data.status)
     },
 
     onError: (error: any) => {
-      setActivationStatusCode(error.response.status)
+      const statusCode = error.response.status
+      if (statusCode === 409) {
+        setActivationStatusCode(error.response.status)
+      }
     },
+
+    retry: false,
   })
 
   useEffect(() => {
@@ -28,7 +36,35 @@ const useActivationRequest = () => {
     }
   }, [activateAccountMutation, searchParams])
 
-  return { activationStatusCode }
+  const activationResultsStatusData = useMemo(() => {
+    return {
+      200: {
+        text: t('activation-success'),
+        redirect: '/auth/log-in',
+        image: celebration,
+      },
+      409: {
+        text: t('account_already_activated'),
+        redirect: '/auth/log-in',
+        image: teamDiscussing,
+      },
+      fail: {
+        text: t('activation_fail'),
+        image: stressedMan,
+        redirect: '/',
+      },
+    }
+  }, [t])
+
+  const activationResultToShow = useMemo(() => {
+    return activationResultsStatusData[
+      (activationStatusCode
+        ? activationStatusCode
+        : 'fail') as keyof typeof activationResultsStatusData
+    ]
+  }, [activationResultsStatusData, activationStatusCode])
+
+  return { activationResultToShow, isLoading, t }
 }
 
 export default useActivationRequest
