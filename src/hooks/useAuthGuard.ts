9a69d62c057bc252435e94a1b81ad8e126faa78d
Saba-Var <vartasashvili94@gmail.com>
2023-10-12 11:43:01 +0400

    fix: set access token using dispatch

--- src/hooks/useAuthGuard.ts
+++ src/hooks/useAuthGuard.ts
@@ -1,20 +1,22 @@
 import { StoreRootState, setAccessToken } from '@/store'
+import { useSelector, useDispatch } from 'react-redux'
 import { refreshTokenRequest } from '@/services'
 import { useQuery } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import Cookies from 'js-cookie'
 
 export const useAuthGuard = () => {
+  const dispatch = useDispatch()
   const { accessToken } = useSelector((state: StoreRootState) => state.authentication)
   const navigate = useNavigate()
   const refreshToken = Cookies.get('refreshToken') as string
 
   const { data } = useQuery(['refreshedAccessToken'], () => refreshTokenRequest(refreshToken), {
-    onSuccess: (data) => setAccessToken(data.data.accessToken),
+    onSuccess: (data) => dispatch(setAccessToken(data.data.accessToken)),
     onError: () => navigate('/auth/log-in'),
     enabled: !accessToken && !!refreshToken,
+    retry: 2,
   })
 
   useEffect(() => {
